20a009ee50ca652aa9f0345f10aae9d850c9bad1
spyrocode <spiralnet.mx@gmail.com>
2018-08-08 08:40:27 -0500

    agregarndo idioma a correo

--- correo.php
+++ correo.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Intr@net Credicor</title>
+    <link rel="shortcut icon" href="http://wwww.ejemplo.org/img/favicon.ico" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+   
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
 <?php
 // $to="efren.almanza@credicor.com.mx";
 // $message="prueba mail";
@@ -26,3 +42,5 @@ else{
 $mensaje= "! Error en el envio de mail Contacte a su Soporte";}
 $banderamsj=1;
 ?>
+</body>
+</html>
