33b6f843507c43d5d2a9b00af4d0439e43e88ec2
spyrocode <spiralnet.mx@gmail.com>
2018-08-07 19:47:41 -0500

    revista mensual

--- revista.php
+++ revista.php
@@ -2,8 +2,8 @@
     require_once 'header.php';
     //////inicio de contenido
 ?>    
-    <iframe src="http://data.axmag.com/data/201807/20180712/U138524_F483944/FLASH/index.html" width="925" height="540" scr="" frameborder="0" allowfullscreen></iframe>
-    
+    <iframe src="https://data.axmag.com/data/201808/20180807/U138524_F485330/FLASH/index.html" width="925" height="540" scr="" frameborder="0" allowfullscreen></iframe>
+                 
 <?php 
     /////fin de contenido
     require_once 'footer.php';
