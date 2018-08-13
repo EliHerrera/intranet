5a6e92d1acc9cd26d6f8803ef096f73f95d7abfa
spyrocode <spiralnet.mx@gmail.com>
2018-08-13 18:50:43 -0500

    quitar bloqueo de pruebas del mail

--- mailing.php
+++ mailing.php
@@ -99,7 +99,7 @@ if (!empty($_POST['asunto'])) {
         sibware.usuarios A
     INNER JOIN sibware.personal B ON B.IDUsuario = A.ID
     WHERE
-        B.`status` = 'S' and B.ID=31"; 
+        B.`status` = 'S'"; 
               
             break;    
     }
