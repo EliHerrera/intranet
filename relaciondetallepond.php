f877c344cee9ee17d58e25ca1ab1dbc25ebeeab9
spyrocode <spiralnet.mx@gmail.com>
2018-10-04 18:32:36 -0500

    ponderacio de tazas de AP

--- relaciondetallepond.php
+++ relaciondetallepond.php
@@ -425,7 +425,178 @@
         AND B.IDTipoCliente<>2
         AND A.Periodo = $periodo
         AND A.yy = $yy");
-            break;                    
+            break;
+        case 15:
+            $titulo='FINANCIAMIENTO RURAL VIGENTE';    
+            $queryResult=$pdo->query("SELECT
+            CONCAT(
+                B.Nombre,
+                ' ',
+                B.Apellido1,
+                ' ',
+                B.Apellido2
+            ) AS Cliente,
+            CONCAT('CR-', LPAD(C.Folio, 6, 0)) AS Folio,
+            D.Renglon,
+            A.SaldoCap,
+            A.Tasa,
+            A.PAdicional,
+            A.TasaTot,
+            A.Interes,
+            A.InteresFND
+        FROM
+            Intranet.relacion_tasa_pond A
+        INNER JOIN sibware.2_cliente B ON A.IDCliente = B.ID
+        INNER JOIN sibware.2_contratos C ON A.IDContrato = C.ID
+        INNER JOIN sibware.2_contratos_disposicion D ON A.IDDisposicion = D.ID
+        WHERE
+            A.Producto = 'CR'
+        AND A.IDMoneda = 1
+        AND A.Empresa = 'CMU'
+        AND D.IDOrigenRecursos=2
+        AND A.TipoCartera='G'
+        AND A.InteresFND>0
+        AND A.Periodo = $periodo
+        AND A.yy = $yy");
+            break; 
+        case 16:
+            $titulo='AP FUTUROS';    
+            $queryResult=$pdo->query("SELECT
+            CONCAT(
+                B.Nombre,
+                ' ',
+                B.Apellido1,
+                ' ',
+                B.Apellido2
+            ) AS Cliente,
+            CONCAT('AP-', LPAD(C.Folio, 6, 0)) AS Folio,
+            A.TRenta,
+            A.TotalR,
+            A.RentaR,
+            A.RentaM,
+            A.IVA,
+            A.iTR,
+            C.ValorBien,
+            C.Deposito,
+            D.SaldoFinal AS VR,
+            A.iPC,
+            A.TasaTot
+        FROM
+            Intranet.relacion_tasa_pond A
+        INNER JOIN sibware.2_cliente B ON A.IDCLiente = B.ID
+        INNER JOIN sibware.2_ap_contrato C ON A.IDContrato = C.ID
+        INNER JOIN sibware.2_ap_valorresidual D ON A.IDContrato = D.IDContrato
+        WHERE
+            A.Producto = 'AP'
+        AND A.IDMoneda = 1
+        AND A.Empresa = 'CMU'
+        AND A.Periodo = $periodo
+        AND A.yy = $yy");
+            break;  
+        case 17:
+            $titulo='AP FUTUROS S/PR';    
+            $queryResult=$pdo->query("SELECT
+            CONCAT(
+                B.Nombre,
+                ' ',
+                B.Apellido1,
+                ' ',
+                B.Apellido2
+            ) AS Cliente,
+            CONCAT('AP-', LPAD(C.Folio, 6, 0)) AS Folio,
+            A.TRenta,
+            A.TotalR,
+            A.RentaR,
+            A.RentaM,
+            A.IVA,
+            A.iTR,
+            C.ValorBien,
+            C.Deposito,
+            D.SaldoFinal AS VR,
+            A.iPC,
+            A.TasaTot
+        FROM
+            Intranet.relacion_tasa_pond A
+        INNER JOIN sibware.2_cliente B ON A.IDCLiente = B.ID
+        INNER JOIN sibware.2_ap_contrato C ON A.IDContrato = C.ID
+        INNER JOIN sibware.2_ap_valorresidual D ON A.IDContrato = D.IDContrato
+        WHERE
+            A.Producto = 'AP'
+        AND A.IDMoneda = 1
+        AND A.Empresa = 'CMU'
+        AND B.IDTipoCliente<>2
+        AND A.Periodo = $periodo
+        AND A.yy = $yy");
+            break;  
+        case 18:
+            $titulo='AP FUTUROS';    
+            $queryResult=$pdo->query("SELECT
+            CONCAT(
+                B.Nombre,
+                ' ',
+                B.Apellido1,
+                ' ',
+                B.Apellido2
+            ) AS Cliente,
+            CONCAT('AP-', LPAD(C.Folio, 6, 0)) AS Folio,
+            A.TRenta,
+            A.TotalR,
+            A.RentaR,
+            A.RentaM,
+            A.IVA,
+            A.iTR,
+            C.ValorBien,
+            C.Deposito,
+            D.SaldoFinal AS VR,
+            A.iPC,
+            A.TasaTot
+        FROM
+            Intranet.relacion_tasa_pond A
+        INNER JOIN sibware.2_cliente B ON A.IDCLiente = B.ID
+        INNER JOIN sibware.2_ap_contrato C ON A.IDContrato = C.ID
+        INNER JOIN sibware.2_ap_valorresidual D ON A.IDContrato = D.IDContrato
+        WHERE
+            A.Producto = 'AP'
+        AND A.IDMoneda = 1
+        AND A.Empresa = 'CMA'
+        AND A.Periodo = $periodo
+        AND A.yy = $yy");
+            break;  
+        case 19:
+            $titulo='AP FUTUROS S/PR';    
+            $queryResult=$pdo->query("SELECT
+            CONCAT(
+                B.Nombre,
+                ' ',
+                B.Apellido1,
+                ' ',
+                B.Apellido2
+            ) AS Cliente,
+            CONCAT('AP-', LPAD(C.Folio, 6, 0)) AS Folio,
+            A.TRenta,
+            A.TotalR,
+            A.RentaR,
+            A.RentaM,
+            A.IVA,
+            A.iTR,
+            C.ValorBien,
+            C.Deposito,
+            D.SaldoFinal AS VR,
+            A.iPC,
+            A.TasaTot
+        FROM
+            Intranet.relacion_tasa_pond A
+        INNER JOIN sibware.2_cliente B ON A.IDCLiente = B.ID
+        INNER JOIN sibware.2_ap_contrato C ON A.IDContrato = C.ID
+        INNER JOIN sibware.2_ap_valorresidual D ON A.IDContrato = D.IDContrato
+        WHERE
+            A.Producto = 'AP'
+        AND A.IDMoneda = 1
+        AND A.Empresa = 'CMA'
+        AND B.IDTipoCliente<>2
+        AND A.Periodo = $periodo
+        AND A.yy = $yy");
+            break;                                    
         default:
         echo "<div class='alert alert-danger'>";
         echo "    <strong>Aviso!</strong> No existe Informacion Para Mostrar";
@@ -441,12 +612,32 @@
 </div>   
 <h3>Relacion Detallada <?PHP echo $titulo." ".date("M-Y", mktime(0, 0, 0, $periodo, 1, $yy)); ?></h3>
 <table class="table">
-<tr><th>No.</th><th>Cliente</th><th>Folio</th><th>Disp</th><th>Saldo Capital</th><th>Tasa</th><th>S.T.</th><th>Tasa T.</th><th>Interes</th><th>Interes FND</th></tr>
 <?PHP
+if ($tipo<15) {
+    echo "<tr><th>No.</th><th>Cliente</th><th>Folio</th><th>Disp</th><th>Saldo Capital</th><th>Tasa</th><th>S.T.</th><th>Tasa T.</th><th>Interes</th><th>Interes FND</th></tr>";
     while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
         $fila++;
         echo "<tr><td>".$fila."</td><td>".$row['Cliente']."</td><td>".$row['Folio']."</td><td>".$row['Renglon']."</td><td>".number_format($row['SaldoCap'],2)."</td><td>".$row['Tasa']."</td><td>".$row['PAdicional']."</td><td>".$row['TasaTot']."</td><td>".number_format($row['Interes'],2)."</td><td>".number_format($row['InteresFND'],2)."</td></tr>";
     }
+
+}
+elseif($tipo>15 && $tipo <20) {
+    echo "<tr><th>No.</th><th>Cliente</th><th>Folio</th><th>Total Renta</th><th>Rentas Pendientes </th><th>Renta</th><th>Ingresos Por Rentas</th><th>MOI</th><th>Depositos</th><th>VR</th><th>Saldo</th><th>Utilidad</th><th>Tasa Total</th></tr>";
+while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
+        $fila++;
+        $renta=$row['RentaM'];
+        $iva=$row['IVA'];
+        $totalRenta=$renta+$iva;
+        $moi=$row['ValorBien'];
+        $itr=$row['iTR'];
+        $iPC=$row['iPC'];
+        $itr=$itr+$iPC;
+        $deposito=$row['Deposito'];
+        $vr=$row['VR'];
+        $utilidad=$itr+$deposito+$vr-$moi;
+        echo "<tr><td>".$fila."</td><td>".$row['Cliente']."</td><td>".$row['Folio']."</td><td>".$row['TotalR']."</td><td>".$row['RentaR']."</td><td>".number_format($renta,2)."</td><td>".number_format($itr,2)."</td><td>".number_format($moi,2)."</td><td>".number_format($deposito,2)."</td><td>".number_format($vr,2)."</td><td>".number_format($row['iPC'],2)."</td><td>".number_format($utilidad,2)."</td><td>".number_format($row['TasaTot'],2)."</td></tr>";
+    }
+}
 ?>
 </table>
 <?php
--- relacionpond.php
+++ relacionpond.php
@@ -15,6 +15,7 @@
 </div>  
 <h3>Relacion de Tasas Ponderadas del mes de : <?PHP echo date("M-Y", mktime(0, 0, 0, $periodo, 1, $yy)); ?></h3>
 <table class="table">
+<tr><th colspan='7'>CREDICOR MEXICANO UNION DE CREDITO SA DE CV</th></tr>
 <tr><th>Producto</th><th>Tipo</th><th>Capital</th><th>Intereses</th><th>Tasa Pond.</th><th>Mes</th><th>Ejercicio</th></tr>
 <?PHP
 $queryResult=$pdo->query("SELECT SUM(A.SaldoCap) as Cap,SUM(A.Interes) as Inte FROM Intranet.relacion_tasa_pond A WHERE A.Producto='CR' AND A.IDMoneda=1 AND A.Empresa='CMU' AND A.Periodo=$periodo AND A.yy=$yy ");
@@ -40,6 +41,14 @@ while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
     $interes=$row['Inte'];
     $tasap=(($interes/$capital)*(360/$dpond))*100;
 echo "<tr><td>CR</td><td><a href='relaciondetallepond.php?tipo=3&periodo=".$periodo."&yy=".$yy."'>FINANCIAMIENTO RURAL</a></td><td>".number_format($capital,2)."</td><td>".number_format($interes,2)."</td><td>".number_format($tasap,2)."</td><td>".$periodo."</td><td>".$yy."</td></tr>";    
+}
+$queryResult=$pdo->query("SELECT SUM(A.SaldoCap) as Cap,SUM(A.InteresFND) as Inte FROM Intranet.relacion_tasa_pond A INNER JOIN sibware.2_contratos_disposicion B ON A.IDDisposicion=B.ID WHERE A.Producto='CR' AND A.IDMoneda=1 AND A.TipoCartera='G' AND A.Empresa='CMU' AND B.IDOrigenRecursos=2 AND A.Periodo=$periodo AND A.yy=$yy AND A.InteresFND>0 ");
+
+while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
+    $capital=$row['Cap'];
+    $interes=$row['Inte'];
+    $tasap=(($interes/$capital)*(360/$dpond))*100;
+echo "<tr><td>CR</td><td><a href='relaciondetallepond.php?tipo=15&periodo=".$periodo."&yy=".$yy."'>FINANCIAMIENTO RURAL VIGENTE</a></td><td>".number_format($capital,2)."</td><td>".number_format($interes,2)."</td><td>".number_format($tasap,2)."</td><td>".$periodo."</td><td>".$yy."</td></tr>";    
 }
 $queryResult=$pdo->query("SELECT SUM(A.SaldoCap) as Cap,SUM(A.Interes) as Inte FROM Intranet.relacion_tasa_pond A INNER JOIN sibware.2_contratos_disposicion B ON A.IDDisposicion=B.ID WHERE A.Producto='CR' AND A.IDMoneda=2 AND A.Empresa='CMU' AND A.Periodo=$periodo AND A.yy=$yy ");
 
@@ -118,6 +127,28 @@ while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
     $tasap=(($interes/$capital)*(360/$dpond))*100;
 echo "<tr><td>IN</td><td><a href='relaciondetallepond.php?tipo=12&periodo=".$periodo."&yy=".$yy."'>PASIVA DLS S/PR(CC.E. en Dls)</a> </td><td>".number_format($capital,2)."</td><td>".number_format($interes,2)."</td><td>".number_format($tasap,2)."</td><td>".$periodo."</td><td>".$yy."</td></tr>";    
 }
+echo "<tr><th>Producto</th><th>Tipo</th><th>Tasa Total</th><th>No. de Ctos</th><th>Tasa Prom.</th><th>Mes</th><th>Ejercicio</th></tr>";
+$queryResult=$pdo->query("SELECT * FROM Intranet.relacion_tasa_pond A INNER JOIN sibware.2_cliente B ON A.IDCLiente=B.ID WHERE A.Producto='AP' AND A.IDMoneda=1 AND A.Empresa='CMU' AND A.Periodo=$periodo AND A.yy=$yy ");
+$num_ctos = $queryResult->rowCount(); 
+$queryResult=$pdo->query("SELECT SUM(A.TasaTot) as TasaTotal FROM Intranet.relacion_tasa_pond A INNER JOIN sibware.2_cliente B ON A.IDCLiente=B.ID WHERE A.Producto='AP' AND A.IDMoneda=1 AND  A.Empresa='CMU' AND A.Periodo=$periodo AND A.yy=$yy ");
+while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
+    $tasatotalAP=$row['TasaTotal'];
+    $tasaPromAP=$tasatotalAP/$num_ctos;
+    
+echo "<tr><td>AP</td><td><a href='relaciondetallepond.php?tipo=16&periodo=".$periodo."&yy=".$yy."'>AP FUTUROS</a> </td><td>".number_format($tasatotalAP,2)."</td><td>".$num_ctos."</td><td>".number_format($tasaPromAP,2)."</td><td>".$periodo."</td><td>".$yy."</td></tr>";    
+}
+$queryResult=$pdo->query("SELECT * FROM Intranet.relacion_tasa_pond A INNER JOIN sibware.2_cliente B ON A.IDCLiente=B.ID WHERE A.Producto='AP' AND A.IDMoneda=1 AND B.IDTipoCliente<>2 AND A.Empresa='CMU' AND A.Periodo=$periodo AND A.yy=$yy ");
+$num_ctos = $queryResult->rowCount(); 
+$queryResult=$pdo->query("SELECT SUM(A.TasaTot) as TasaTotal FROM Intranet.relacion_tasa_pond A INNER JOIN sibware.2_cliente B ON A.IDCLiente=B.ID WHERE A.Producto='AP' AND A.IDMoneda=1 AND B.IDTipoCliente<>2 AND A.Empresa='CMU' AND A.Periodo=$periodo AND A.yy=$yy ");
+while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
+    $tasatotalAP=$row['TasaTotal'];
+    $tasaPromAP=$tasatotalAP/$num_ctos;
+    
+echo "<tr><td>AP</td><td><a href='relaciondetallepond.php?tipo=17&periodo=".$periodo."&yy=".$yy."'>AP FUTUROS S/PR</a> </td><td>".number_format($tasatotalAP,2)."</td><td>".$num_ctos."</td><td>".number_format($tasaPromAP,2)."</td><td>".$periodo."</td><td>".$yy."</td></tr>";    
+}
+echo "</table>";
+echo "<table class='table'>";
+echo "<tr><th colspan='7'>CREDICOR MEXICANO ARRENDADORA SA DE CV</th></tr>";
 $queryResult=$pdo->query("SELECT SUM(A.SaldoCap) as Cap,SUM(A.Interes) as Inte FROM Intranet.relacion_tasa_pond A WHERE A.Producto='VP' AND A.IDMoneda=1 AND A.Empresa='CMA' AND A.Periodo=$periodo AND A.yy=$yy ");
 
 while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
@@ -133,6 +164,25 @@ while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
     $interes=$row['Inte'];
     $tasap=(($interes/$capital)*(360/$dpond))*100;
 echo "<tr><td>VP</td><td><a href='relaciondetallepond.php?tipo=14&periodo=".$periodo."&yy=".$yy."'>VENTA PLAZO S/PR</a> </td><td>".number_format($capital,2)."</td><td>".number_format($interes,2)."</td><td>".number_format($tasap,2)."</td><td>".$periodo."</td><td>".$yy."</td></tr>";    
+}
+echo "<tr><th>Producto</th><th>Tipo</th><th>Tasa Total</th><th>No. de Ctos</th><th>Tasa Prom.</th><th>Mes</th><th>Ejercicio</th></tr>";
+$queryResult=$pdo->query("SELECT * FROM Intranet.relacion_tasa_pond A INNER JOIN sibware.3_cliente B ON A.IDCLiente=B.ID WHERE A.Producto='AP' AND A.IDMoneda=1 AND A.Empresa='CMA' AND A.Periodo=$periodo AND A.yy=$yy ");
+$num_ctos = $queryResult->rowCount(); 
+$queryResult=$pdo->query("SELECT SUM(A.TasaTot) as TasaTotal FROM Intranet.relacion_tasa_pond A INNER JOIN sibware.3_cliente B ON A.IDCLiente=B.ID WHERE A.Producto='AP' AND A.IDMoneda=1 AND  A.Empresa='CMA' AND A.Periodo=$periodo AND A.yy=$yy ");
+while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
+    $tasatotalAP=$row['TasaTotal'];
+    $tasaPromAP=$tasatotalAP/$num_ctos;
+    
+echo "<tr><td>AP</td><td><a href='relaciondetallepond.php?tipo=18&periodo=".$periodo."&yy=".$yy."'>AP FUTUROS</a> </td><td>".number_format($tasatotalAP,2)."</td><td>".$num_ctos."</td><td>".number_format($tasaPromAP,2)."</td><td>".$periodo."</td><td>".$yy."</td></tr>";    
+}
+$queryResult=$pdo->query("SELECT * FROM Intranet.relacion_tasa_pond A INNER JOIN sibware.3_cliente B ON A.IDCLiente=B.ID WHERE A.Producto='AP' AND A.IDMoneda=1 AND B.IDTipoCliente<>2 AND A.Empresa='CMA' AND A.Periodo=$periodo AND A.yy=$yy ");
+$num_ctos = $queryResult->rowCount(); 
+$queryResult=$pdo->query("SELECT SUM(A.TasaTot) as TasaTotal FROM Intranet.relacion_tasa_pond A INNER JOIN sibware.3_cliente B ON A.IDCLiente=B.ID WHERE A.Producto='AP' AND A.IDMoneda=1 AND B.IDTipoCliente<>2 AND A.Empresa='CMA' AND A.Periodo=$periodo AND A.yy=$yy ");
+while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
+    $tasatotalAP=$row['TasaTotal'];
+    $tasaPromAP=$tasatotalAP/$num_ctos;
+    
+echo "<tr><td>AP</td><td><a href='relaciondetallepond.php?tipo=19&periodo=".$periodo."&yy=".$yy."'>AP FUTUROS S/PR</a> </td><td>".number_format($tasatotalAP,2)."</td><td>".$num_ctos."</td><td>".number_format($tasaPromAP,2)."</td><td>".$periodo."</td><td>".$yy."</td></tr>";    
 }
 ?>
 </table>
