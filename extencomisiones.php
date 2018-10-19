a31f49d0157777da06c8bd6fca10e4d9d471e6d9
spyrocode <spiralnet.mx@gmail.com>
2018-10-19 09:09:45 -0500

    asignacion y calculo de comisiones ext

--- extencomisiones.php
+++ extencomisiones.php
@@ -5,7 +5,7 @@
     define("pComi", 2.5);
     define("metaCR",20000000);
     define("metaAP",5000000);
-    define("metaVP",500000);
+    define("metaVP",5000000);
     define(pcumpli,90);
     if (!empty($_GET['idcom'])) {
         $deletequery=$pdo->prepare("DELETE FROM Intranet.com_extensionistas WHERE ID=$_GET[idcom]");
@@ -15,26 +15,100 @@
         echo "</div>";
     }
     if (!empty($_POST['calcular'])) {
-        $accr=$_POST['accr'];
-        $acaccr=$_POST['acaccr'];
-        $acap=$_POST['acap'];
-        $acacap=$_POST['acacap'];
-        $acvp=$_POST['acvp'];
-        $acacvp=$_POST['acacvp'];
+        
         $periodo=$_POST['periodo'];
         $yy=$_POST['yy'];
         $queryResult=$pdo->query("SELECT * FROM Intranet.param_com_extensionistas");
         while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
             $idpersonalcom=$row['IDPersonal'];
-            $porc=$row['porc'];
+            $porcc=$row['porcc'];
+            $porcp=$row['porcp'];
+
             $queryResult2=$pdo->query("SELECT * FROM Intranet.com_extensionistas WHERE yy=$yy AND periodo=$periodo AND IDPersonal=$idpersonalcom");
             $row_count = $queryResult2->rowCount(); 
             if ($row_count==0) {
-                $comcr=$acaccr*($porc/100);
-                $comap=$acacap*($porc/100);
-                $comvp=$acacvp*($porc/100);
-                $insertquery=$pdo->prepare("INSERT INTO Intranet.com_extensionistas (IDPersonal,porc,accr,acaccr,comcr,acap,acacap,comap,acvp,acacvp,comvp,periodo,yy,status) VALUES ($idpersonalcom,$porc,$accr,$acaccr,$comcr,$acap,$acacap,$comap,$acvp,$acacvp,$comvp,$periodo,$yy,1)");
+                $queryResult3=$pdo->query("SELECT B.Autorizado,B.PAPertura,B.NAPertura,A.IDPersonal FROM Intranet.asignacion_ctos_ext A INNER JOIN sibware.2_contratos B ON A.IDContrato=B.ID WHERE A.periodo=$periodo AND A.yy=$yy AND Producto='cr'");
+                //var_dump($queryResult3);
+                $row_count2 = $queryResult3->rowCount(); 
+                if ($row_count2==0) {
+                    $accr=0;
+                    $acaccr=0;
+                }else{
+                        while ($row=$queryResult3->fetch(PDO::FETCH_ASSOC)) {
+                            $autorizado=$row['Autorizado'];
+                            $pAp=$row['PAPertura'];
+                            $mAp=$row['NAPertura'];
+                            $idpersonalasignado=$row['IDPersonal'];
+                            $pCalcular=$pAp*100/pComi;
+                            $montoCal=($pCalcular/100)*$autorizado;
+                            $accr=$accr+$autorizado;
+                            $acaccr=$acaccr+$montoCal;
+                        }
+                    }        
+                $queryResult3=$pdo->query("SELECT B.Saldo as Autorizado,B.PAPertura,B.NAPertura,A.IDPersonal FROM Intranet.asignacion_ctos_ext A INNER JOIN sibware.2_ap_contrato B ON A.IDContrato=B.ID WHERE A.periodo=$periodo AND A.yy=$yy AND Producto='apu'");
+                $row_count2 = $queryResult3->rowCount();
+                if ($row_count2==0) {
+                    $acap=0;
+                    $acacap=0;
+                }else{
+                    while ($row=$queryResult3->fetch(PDO::FETCH_ASSOC)) {
+                            $autorizado=$row['Autorizado'];
+                            $pAp=$row['PAPertura'];
+                            $mAp=$row['NAPertura'];
+                            $idpersonalasignado=$row['IDPersonal'];
+                            $pCalcular=$pAp*100/pComi;
+                            $montoCal=($pCalcular/100)*$autorizado;
+                            $acap=$acap+$autorizado;
+                            $acacap=$acacap+$montoCal;
+                    }
+                }    
+                
+                $queryResult3=$pdo->query("SELECT B.Saldo as Autorizado,B.PAPertura,B.NAPertura,A.IDPersonal FROM Intranet.asignacion_ctos_ext A INNER JOIN sibware.3_ap_contrato B ON A.IDContrato=B.ID WHERE A.periodo=$periodo AND A.yy=$yy AND Producto='ap'");
+                $row_count2 = $queryResult3->rowCount();
+                $row_count2 = $queryResult3->rowCount();
+                if ($row_count2==0) {
+                    $acap=0;
+                    $acacap=0;
+                }else{
+                        while ($row=$queryResult3->fetch(PDO::FETCH_ASSOC)) {
+                            $autorizado=$row['Autorizado'];
+                            $pAp=$row['PAPertura'];
+                            $mAp=$row['NAPertura'];
+                            $idpersonalasignado=$row['IDPersonal'];
+                            $pCalcular=$pAp*100/pComi;
+                            $montoCal=($pCalcular/100)*$autorizado;
+                            $acap=$acap+$autorizado;
+                            $acacap=$acacap+$montoCal;
+                        }
+                    }    
+                $queryResult3=$pdo->query("SELECT B.SaldoFinal as Autorizado,B.PAPertura,B.NAPertura,A.IDPersonal FROM Intranet.asignacion_ctos_ext A INNER JOIN sibware.3_vp_contrato B ON A.IDContrato=B.ID WHERE A.periodo=$periodo AND A.yy=$yy AND Producto='vp'");
+                $row_count2 = $queryResult3->rowCount();
+                if ($row_count2==0) {
+                    $acvp=0;
+                    $acacvp=0;
+                }else{
+                        while ($row=$queryResult3->fetch(PDO::FETCH_ASSOC)) {
+                            $autorizado=$row['Autorizado'];
+                            $pAp=$row['PAPertura'];
+                            $mAp=$row['NAPertura'];
+                            $idpersonalasignado=$row['IDPersonal'];
+                            $pCalcular=$pAp*100/pComi;
+                            $montoCal=($pCalcular/100)*$autorizado;
+                            $acvp=$acvp+$autorizado;
+                            $acacvp=$acacvp+$montoCal;
+                        }
+                    }
+                if ($idpersonalcom==$idpersonalasignado) {
+                    $porc=$porcp;
+                }else{
+                    $porc=$porcc;
+                }  
+                $comcr=$acaccr*($porc/100);   
+                $comvp=$acacvp*($porc/100);   
+                $comap=$acacap*($porc/100);   
+                $insertquery=$pdo->prepare("INSERT INTO Intranet.com_extensionistas (IDPersonal,porcp,porcc,accr,acaccr,comcr,acap,acacap,comap,acvp,acacvp,comvp,periodo,yy,status) VALUES ($idpersonalcom,$porcp,$porcc,$accr,$acaccr,$comcr,$acap,$acacap,$comap,$acvp,$acacvp,$comvp,$periodo,$yy,1)");
                 $insertquery->execute(); 
+                //var_dump($insertquery);
                
             }
 
@@ -288,20 +362,20 @@
 ?>
 <form action="extencomisiones.php" method="post">
     <input type="text" name="accr" id="accr" value="<?php echo $acumcolocado  ?>" readonly="true" hidden="true">
-    <input type="text" name="acaccr" id="acaccr" value="<?php echo $acumPCalcular ?>" readonly="true" hidden="true">
+    <input type="text" name="acaccr" id="acaccr" value="<?php echo $acumPCalcular ?>" readonly="true" hidden="true" >
     <input type="text" name="acap" id="acap" value="<?php echo $acumcolocadoAP  ?>" readonly="true" hidden="true">
-    <input type="text" name="acacap" id="acacap" value="<?php echo $acumPCalcularAP  ?>" readonly="true" hidden="true">
+    <input type="text" name="acacap" id="acacap" value="<?php echo $acumPCalcularAP  ?>" readonly="true" hidden="true" >
     <input type="text" name="acvp" id="acvp" value="<?php echo $acumcolocadoVP ?>" readonly="true" hidden="true">
-    <input type="text" name="acacvp" id="acacvp" value="<?php echo $acumPCalcularVP  ?>" readonly="true" hidden="true">
+    <input type="text" name="acacvp" id="acacvp" value="<?php echo $acumPCalcularVP  ?>" readonly="true" hidden="true" >
     <input type="text" name="periodo" id="periodo" value="<?php echo $periodo ?>" readonly="true" hidden="true">
     <input type="text" name="yy" id="yy" value="<?php echo $yy ?>" readonly="true" hidden="true">
     <input type="submit" value="calcular" class="button" name="calcular" id="calcular">
 </form>
 <h3>Relacion de Comisiones Pagar</h3>
 <table class="table">
-<tr><th>Nombre</th><th>%</th><th>Creditos</th><th>Arrendamientos</th><th>Venta PLazo</th><th>Total</th><th>Acciones</th></tr>
+<tr><th>Nombre</th><th>%P</th><th>%C</th><th>Creditos</th><th>Arrendamientos</th><th>Venta PLazo</th><th>Total</th><th>Acciones</th></tr>
 <?php
-    $queryResult=$pdo->Query("SELECT A.ID,CONCAT(B.Nombre,' ',B.Apellido1,' ',B.Apellido2) as ejecutivo, C.alias as personal , A.porc,A.comcr,A.comap,A.comvp,A.status FROM Intranet.com_extensionistas A INNER JOIN sibware.personal B ON A.IDpersonal=B.ID INNER JOIN Intranet.param_com_extensionistas C ON A.IDPersonal=C.IDPersonal WHERE periodo=$periodo AND yy=$yy");
+    $queryResult=$pdo->Query("SELECT A.ID,CONCAT(B.Nombre,' ',B.Apellido1,' ',B.Apellido2) as ejecutivo, C.alias as personal , A.porcp,A.porcc,A.comcr,A.comap,A.comvp,A.status FROM Intranet.com_extensionistas A INNER JOIN sibware.personal B ON A.IDpersonal=B.ID INNER JOIN Intranet.param_com_extensionistas C ON A.IDPersonal=C.IDPersonal WHERE periodo=$periodo AND yy=$yy");
     while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
         $comap=$row['comap'];
         $comcr=$row['comcr'];
@@ -311,7 +385,7 @@
         $totalap=$totalap+$comap;
         $totalvp=$totalvp+$comvp;
         $idcom=$row['ID'];
-        echo "<tr><td>".$row['personal']."</td><td>".$row['porc']."</td><td>".number_format($comcr,2)."</td><td>".number_format($comap,2)."</td><td>".number_format($comvp,2)."</td><td>".number_format($total,2)."</td>";
+        echo "<tr><td>".$row['personal']."</td><td>".$row['porcp']."</td><td>".$row['porcc']."</td><td>".number_format($comcr,2)."</td><td>".number_format($comap,2)."</td><td>".number_format($comvp,2)."</td><td>".number_format($total,2)."</td>";
         if ($row['status']==1) {
             echo "<td><a href='extencomisiones.php?idcom=".$idcom."'><img src='img/icons/delete.png'></a><img src='img/icons/aprove.png'></td>";
         }elseif ($row['status']>1) {
@@ -339,7 +413,7 @@
         $classvp='success';
     }
 ?>
-<tr><td>Restante</td><td></td><td class="<?php echo $classcr ?>"><?PHP echo  number_format($rescr,2) ?></td><td class=" <?php echo $classap ?>"><?PHP echo number_format($resap,2) ?></td><td class=" <?php echo $classvp ?>"><?PHP echo  number_format($resvp,2) ?></td><td></td></tr>
+<tr><td colspan="2">Remanente por Utilizar</td><td></td><td class="<?php echo $classcr ?>"><?PHP echo  number_format($rescr,2) ?></td><td class=" <?php echo $classap ?>"><?PHP echo number_format($resap,2) ?></td><td class=" <?php echo $classvp ?>"><?PHP echo  number_format($resvp,2) ?></td><td></td></tr>
 </table>
 <?php
     }
