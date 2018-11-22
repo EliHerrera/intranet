62a521112a206d8b4dd90fac9176653d6b778b15
spyrocode <spiralnet.mx@gmail.com>
2018-11-22 11:12:11 -0600

    revista de noviembre

--- revista.php
+++ revista.php
@@ -2,8 +2,8 @@
     require_once 'header.php';
     //////inicio de contenido
 ?>    
-    <iframe src="https://data.axmag.com/data/201810/20181011/U138524_F488800/FLASH/index.html" width="925" height="540" scr="" frameborder="0" allowfullscreen></iframe>
-                
+    <iframe src="https://data.axmag.com/data/201811/20181122/U138524_F490709/FLASH/index.html" width="925" height="540" scr="" frameborder="0" allowfullscreen></iframe>
+                 
                  
 <?php 
     /////fin de contenido
