0475f4ca37843da9226e632cf2081593b8107e6d
spyrocode <spiralnet.mx@gmail.com>
2018-11-29 08:45:49 -0600

    dministracion y gestion de incidencias de tickets des riego, se generan 3 archivos

---
+++ gestionarin.php
@@ -0,0 +1,138 @@
+<?php   
+    require_once 'headerbi.php';
+    require 'menubi.php';
+    $hoy=date('Y-m-d');
+    if (!empty($_POST['guardar'])) {
+        $queryInsert=$pdo->prepare("INSERT INTO Intranet.gestion_riesgos (ID_Ticket,fecha,impacto,seguimiento) VALUES($_POST[idticket],'$hoy','$_POST[impacto]','$_POST[seguimiento]')");
+        $queryInsert->execute();
+        echo "<div class='alert alert-success'>";
+        echo "    <strong>Exito!</strong> Guardado con Exito!";
+        echo "</div>";
+    }
+    if (!empty($_POST['actualizar'])) {
+        $queryUpdate=$pdo->prepare("UPDATE Intranet.gestion_riesgos SET impacto='$_POST[impacto]', seguimiento='$_POST[seguimiento]', fecha='$hoy' WHERE ID_Ticket=$_POST[idticket] ");
+        $queryUpdate->execute();
+        echo "<div class='alert alert-success'>";
+        echo "    <strong>Exito!</strong> Actualizado con Exito!";
+        echo "</div>";
+    }
+    if (!empty($_GET)) {
+        $id_ticket=$_GET['idticket'];
+    }elseif(!empty($_POST)){
+        $id_ticket=$_POST['idticket'];
+
+    }
+        $queryResult=$pdo->query("SELECT
+                                    A.ID_Ticket,
+                                    A.fecha_alta,
+                                    A.folio,
+                                    C.macroproceso,
+                                    A.Mensaje,
+                                    CONCAT(D.Nombre,' ',D.Apellido1,' ',D.Apellido2) as usuario
+                                FROM
+                                    Intranet.ticket A
+                                INNER JOIN Intranet.msj_ticket B ON A.ID_Ticket = B.IDTicket
+                                INNER JOIN Intranet.r_macroproceso C on A.ID_Categoria=C.ID
+                                INNER JOIN sibware.personal D on A.ID_Usuario=D.ID
+                                WHERE
+                                    A.ID_Ticket=$id_ticket");
+                                while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
+                                    $idticket=$row['ID_Ticket'];
+                                    $fecha_alta=$row['fecha_alta'];
+                                    $folio=$row['folio'];
+                                    $macroproceso=$row['macroproceso'];
+                                    $acontecimineto=$row['Mensaje'];
+                                    $usuario=$row['usuario'];
+                                }    
+                                $queryResult2=$pdo->query("SELECT
+                                                A.mensaje
+                                            FROM
+                                                Intranet.msj_ticket A
+                                            
+                                            WHERE
+                                                A.IDTicket = $idticket
+                                                and A.IDUsuario=$idcontraloria
+                                            "); 
+                                            
+                                while ($row=$queryResult2->fetch(PDO::FETCH_ASSOC)) {
+                                        $cambios=$row['mensaje'];
+                                        
+                                } 
+                                $queryResult2=$pdo->query("SELECT
+                                                *
+                                            FROM
+                                                Intranet.gestion_riesgos A
+                                            
+                                            WHERE
+                                                A.ID_Ticket = $idticket
+                                               
+                                            "); 
+                                            
+                                $row_count = $queryResult2->rowCount(); 
+                                while ($row=$queryResult2->fetch(PDO::FETCH_ASSOC)) {
+                                    $impacto=$row['impacto'];
+                                    $seguimiento=$row['seguimiento'];
+                                    
+                                } 
+
+                                   
+    
+    //////inicio de contenido
+?>  
+<h3>Comentarios para la Incidencia : <?php echo $folio; ?></h3> 
+<div class="row">
+  <div class="col-md-4"><?php echo "<strong>Fecha : </strong><p class='text-xs-left'>".$fecha_alta."</p>"; ?></div>
+  <div class="col-md-4"><?php echo "<strong>Proceso : </strong><p class='text-xs-left'>".$macroproceso."</p>"; ?></div>
+  <div class="col-md-4"><?php echo "<strong>Usuario : </strong><p class='text-xs-left'>".$usuario."</p>"; ?></div>
+</div>
+<br>
+<div class="row">
+  <div class="col-md-6"><?php echo "<strong>Acontecimiento : </strong><p class='text-xs-left'>".$acontecimineto."</p>"; ?></div>
+  <div class="col-md-6"><?php echo "<strong>Cambios : </strong><p class='text-xs-left'>".$cambios."</p>"; ?></div>
+</div>
+<div class="row">
+  <div class="col-md-6"><?php echo "<strong>Impacto o Modificacion en Políticas : </strong><p class='text-xs-left'>".$impacto."</p>"; ?></div>
+  <div class="col-md-6"><?php echo "<strong>Seguimiento Contralor Normativo : </strong><p class='text-xs-left'>".$seguimiento."</p>"; ?></div>
+</div>
+<form action="gestionarin.php" method="post">
+    <div class="row">
+        <input type="text" name="idticket" id="idticket" value="<?php echo $idticket ?>" required="true" hidden="true">
+        <div class="col-xs-6">
+            <label for="impacto">Impacto</label><textarea name="impacto" id="impacto" cols="30" rows="5" placeholder="Ingresar informacion aqui" class="form-control" required="true"></textarea>
+        </div>
+        <div class="col-xs-6">
+            <label for="seguimineto">Seguimiento</label><textarea name="seguimiento" id="seguimiento" cols="30" rows="5" placeholder="Ingresar informacion aqui" class="form-control" required="true"></textarea>
+        </div>
+    </div>
+       
+        
+    
+    <?php
+    
+    if($row_count==0){
+    echo "<div class='row'>    
+        <div class='col-xs-3'>
+            <input type='submit' value='Guardar' id='guardar' name='guardar' class='button'>
+        </div>";
+    
+    }elseif($row_count != 0){
+    
+    echo "  <div class='row'>    
+        <div class='col-xs-3'>
+            <input type='submit' value='Actualizar' id='actualizar' name='actualizar' class='button'>
+        </div>";
+    
+    }
+    
+    ?>
+        <div class="col-xs-2">   
+            <a href="incidenciasg.php?b=<?php echo $_GET['b']; ?>&fil=<?php echo $_GET['fil']; ?>" class="button">Regresar</a>
+        </div>
+    </div>
+</form>
+
+
+<?php
+    /////fin de contenido
+    require_once 'footer.php';
+?>
---
+++ incidenciasg.php
@@ -0,0 +1,133 @@
+<?php
+    require_once 'headerbi.php';
+    require 'menubi.php';
+    //////inicio de contenido
+    switch ($_GET['b']) {
+        case 'a':
+           $band='A';
+            break;
+        case 'p':
+            $band='P';
+             break;
+        case 'c':
+             $band='C';
+              break;
+        case 'r':
+              $band='R';
+               break;
+             
+        default:
+            $band='N';
+            break;
+    }
+    if (empty($_GET['fil'])) {
+        $fini='2018-01-01';
+        $ffin='2018-12-31';
+    }else{
+        $queryResult=$pdo->query("SELECT * from Intranet.filtros_bi where valor=$_GET[fil]");
+                while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
+                    $ffin=$row['ffin'];
+                    $fini=$row['fini'];
+                    $texto=$row['texto'];
+                }
+    }
+    if ($band=='R') {
+        $queryResult=$pdo->query("SELECT
+                                    A.ID_Ticket,
+                                    A.fecha_alta,
+                                    A.folio,
+                                    C.macroproceso,
+                                    A.Mensaje
+                                FROM
+                                    Intranet.ticket A
+                                INNER JOIN Intranet.msj_ticket B ON A.ID_Ticket = B.IDTicket
+                                INNER JOIN Intranet.r_macroproceso C on A.ID_Categoria=C.ID
+                                WHERE
+                                    (
+                                        A.fecha_alta BETWEEN '$fini'
+                                        AND '$ffin'
+                                    )
+                                AND B.IDUsuario = $idcontraloria
+                                AND A.ID_Usuario<>$idcontraloria");
+        
+        
+    }
+?> 
+<h3>Relacion de Casos de Riesgo del mes de <?php echo $texto; ?></h3>
+<div class="col-xs-2">   
+    <a href="viewincidencia.php?b=<?php echo $_GET['b']; ?>&fil=<?php echo $_GET['fil']; ?>" class="button">Regresar</a>
+</div>
+<table class="table">
+    <tr><th>Fecha</th><th>Folio</th><th>Proceso</th><th>Acontecimiento</th><th>Area Involucradas</th><th>Cambios o Definicion Final</th><th>Impacto o Modificacion de Politica</th><th>Seguimiento Contralor Normativo</th><th>Acciones</th></tr>
+    <?php
+        while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
+                    $idticket=$row['ID_Ticket'];
+                    $fecha_alta=$row['fecha_alta'];
+                    $folio=$row['folio'];
+                    $macroproceso=$row['macroproceso'];
+                    $acontecimineto=$row['Mensaje'];
+                    echo "<tr><td>".$fecha_alta."</td><td><a href='viewincidencia2.php?idtic=".$idticket."'>".$folio."</a></td><td>".$macroproceso."</td><td>".$acontecimineto."</td>";
+                    $queryResult2=$pdo->query("SELECT
+                                                C.Nombre
+                                            FROM
+                                                Intranet.msj_ticket A
+                                            INNER JOIN sibware.personal B ON A.IDUsuario = B.ID
+                                            INNER JOIN sibware.personal_departamentos C ON B.IDDepartamento = C.ID
+                                            WHERE
+                                                IDTicket = $idticket
+                                            GROUP BY
+                                                A.IDUsuario");
+                    echo "<td>";
+                    echo "<li>";                            
+                    while ($row=$queryResult2->fetch(PDO::FETCH_ASSOC)) {
+                        $area=$row['Nombre'];
+                        echo "<ul>".$area."</ul>";
+                        $areasin=array($area);
+                    }
+                    echo "</li>";
+                    echo "</td>";  
+                    $queryResult3=$pdo->query("SELECT
+                                                A.mensaje
+                                            FROM
+                                                Intranet.msj_ticket A
+                                            
+                                            WHERE
+                                                A.IDTicket = $idticket
+                                                and A.IDUsuario=$idcontraloria
+                                            LIMIT 1"); 
+                    while ($row=$queryResult3->fetch(PDO::FETCH_ASSOC)) {
+                        $cambios=$row['mensaje'];
+                        echo "<td>".$cambios."</td>";
+                    }
+                    $impacto="";
+                    $seguimiento=""; 
+                    $queryResult4=$pdo->query("SELECT
+                                                *
+                                            FROM
+                                                Intranet.gestion_riesgos A
+                                            
+                                            WHERE
+                                                A.ID_Ticket = $idticket
+                                               
+                                            "); 
+                                            
+                                
+                                while ($row=$queryResult4->fetch(PDO::FETCH_ASSOC)) {
+                                    $impacto=$row['impacto'];
+                                    $seguimiento=$row['seguimiento'];
+                                    
+                            }                                                 
+                echo "<td>".$impacto."</td><td>".$seguimiento."</td><td><a href='gestionarin.php?idticket=".$idticket."&b=".$_GET['b']."&fil=".$_GET['fil']."'>Gestionar</a></td></tr>";    
+                }
+    ?>    
+</table>  
+<li>
+    <ul>
+
+    </ul>
+</li>
+
+<?php
+    /////fin de contenido
+    require_once 'footer.php';
+?>
