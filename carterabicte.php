2f554891603747926c586bef8faab472b80bbb01
spyrocode <spiralnet.mx@gmail.com>
2018-12-04 08:36:32 -0600

    bug en tipo de cambio no calculaba en cartera cte

--- carterabicte.php
+++ carterabicte.php
@@ -3,16 +3,20 @@
     require_once 'headerbi.php';
     //////inicio de contenido
     //consulta tipo de cambio
-    $queryResult=$pdo->query("SELECT * FROM sibware.indicador_tipocambio WHERE Fecha='$hoy'");
-    while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
-        $tc=$row['Paridad'];
-    }//consulta tipo de cambio
+    
     require 'estiloconst.php'; 
     require 'menubi.php';
     if (!empty($_POST['hoy'])) {
         $hoy=$_POST['hoy'];
         
     }
+    $queryResult=$pdo->query("SELECT * FROM sibware.indicador_tipocambio WHERE Fecha='$hoy'");
+    var_dump($queryResult);
+    while ($row=$queryResult->fetch(PDO::FETCH_ASSOC)) {
+        $tc=$row['Paridad'];
+    }//consulta tipo de cambio
+    echo $tc;
+    ;
 ?>
 <h3>Relacion de Clientes al dia <?php echo $hoy; ?> </h3> 
 <form action="carterabicte.php" method="post">
